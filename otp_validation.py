ef33468909bbeed123deb9a6aceb70790d1073d5
MohammadLimra <mohammadlimra13@gmail.com>
2026-07-20 14:24:29 +0530

    Update otp_validation.py

--- otp_validation.py
+++ otp_validation.py
@@ -1,25 +1,31 @@
+import os
 import random
 import smtplib
 from email.message import EmailMessage
+from dotenv import load_data  # Import dotenv
+
+# Load the variables from the .env file
+load_dotenv()
 
 otp = ""
 for i in range(6):
     otp += str(random.randint(0,9))
 
-#print(otp)
-
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 
-from_mail = 'mohammadlimra13@gmail.com'
-server.login(from_mail, 'pvvx brca pixz fvqi')
+# Fetch the credentials safely from the environment
+from_mail = os.getenv('EMAIL_USER')
+app_password = os.getenv('EMAIL_PASS')
+
+server.login(from_mail, app_password)
 to_mail = input("Enter your email: ")
 
 msg = EmailMessage()
 msg['Subject'] = "OTP Verification"
 msg['From'] = from_mail
 msg['To'] = to_mail
-msg.set_content("Your  OTP is: " + otp)
+msg.set_content("Your OTP is: " + otp)
 
 server.send_message(msg)
 
@@ -28,4 +34,3 @@ if input_otp == otp:
     print("OTP verified successfully")
 else:
     print("Invalid OTP")
-#print("Email sent successfully")
